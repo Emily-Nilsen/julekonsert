82c40aa10050bac7971fd6256326d9642308a631
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-10-05 17:07:25 +0200

    edit special layout

--- src/components/SpecialLayout.jsx
+++ src/components/SpecialLayout.jsx
@@ -1,42 +1,39 @@
-import Head from 'next/head'
-
 export function SpecialLayout({ children, frontmatter }) {
-  const generateSchema = () => {
-    if (!frontmatter) return null
-
-    const schema = {
-      '@context': 'http://schema.org',
-      '@type': 'Event',
-      name: frontmatter?.title,
-      description: frontmatter?.description,
-      startDate: frontmatter?.date,
-      endDate: new Date(
-        new Date(frontmatter?.date).getTime() + frontmatter?.duration * 60000
-      ).toISOString(),
-      location: {
-        '@type': 'Place',
-        name: frontmatter?.locationName,
-        address: frontmatter?.locationAddress,
-      },
-      image: frontmatter?.image,
-      url: frontmatter?.url,
-      performer: {
-        '@type': 'Person',
-        name: frontmatter?.performer,
-      },
-    }
-
-    return (
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
-      />
-    )
-  }
+  // Generate JSON-LD schema directly here
+  const schema = frontmatter
+    ? {
+        '@context': 'http://schema.org',
+        '@type': 'Event',
+        name: frontmatter.title,
+        description: frontmatter.description,
+        startDate: frontmatter.date,
+        endDate: new Date(
+          new Date(frontmatter.date).getTime() + frontmatter.duration * 60000
+        ).toISOString(),
+        location: {
+          '@type': 'Place',
+          name: frontmatter.locationName,
+          address: frontmatter.locationAddress,
+        },
+        image: frontmatter.image,
+        url: frontmatter.url,
+        performer: {
+          '@type': 'Person',
+          name: frontmatter.performer,
+        },
+      }
+    : null
 
   return (
     <>
-      <Head>{generateSchema()}</Head>
+      <Head>
+        {schema && (
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+          />
+        )}
+      </Head>
       <div className="special-layout">{children}</div>
     </>
   )
