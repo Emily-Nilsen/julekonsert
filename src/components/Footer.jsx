2c9a437ee1ce8cb7d6b0a1f03359f019bcff3aaf
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-01-01 12:39:54 +0100

    add norsk nett link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -89,6 +89,20 @@ export function Footer() {
             &copy; {new Date().getFullYear()} Christmas with Nordic Tenors.
           </p>
         </div>
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="text-center text-sm leading-5 text-white">
+            Utviklet av{' '}
+            <Link
+              href="https://norsknett.no"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cursor-pointer transition-all duration-200 ease-in hover:underline"
+            >
+              Norsk Nett
+            </Link>
+            .
+          </p>
+        </div>
       </div>
     </footer>
   )
