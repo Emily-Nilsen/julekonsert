2a86c7d2800b4d7b2470656db23fbba7b8dcbd28
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-08-30 18:30:59 +0300

    update spotify track url

--- src/components/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer.jsx
@@ -1,10 +1,8 @@
-export function SpotifyPlayer({ spotifyUri }) {
-  const spotifyEmbedUrl = `https://open.spotify.com/embed/${spotifyUri}`
-
+export function SpotifyPlayer() {
   return (
     <div className="mx-left w-full max-w-xl overflow-hidden rounded-xl">
       <iframe
-        src={spotifyEmbedUrl}
+        src="https://open.spotify.com/embed/track/2evTmFdNhNjHLxFHTWFm34?si=b5031ec4924f45c7"
         width="100%"
         height="380"
         frameBorder="0"
